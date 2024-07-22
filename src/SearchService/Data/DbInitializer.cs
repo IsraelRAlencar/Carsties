d3804fc0cf580d6529e2e74d635828e9f2699c33
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-07-22 13:52:55 -0400

    new - adding http communication in the SerchService

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -19,17 +20,12 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if (count == 0)
-        {
-            Console.WriteLine("Seeding database...");
+        using var scope = app.Services.CreateScope();
 
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
 
-            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
-
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-
-            await DB.SaveAsync(items);
-        }
+        Console.Write(items.Count + " items fetched from AuctionService. ");
+        if (items.Count > 0) await DB.SaveAsync(items);
     }
 }
