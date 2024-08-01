03d81b95827a1c833794000f001b81eeea1f395a
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-08-01 17:02:07 -0400

    new - adding types to the project

--- frontend/web-app/app/auctions/Listing.tsx
+++ frontend/web-app/app/auctions/Listing.tsx
@@ -2,8 +2,9 @@ import { error } from 'console';
 import React from 'react'
 import { json } from 'stream/consumers';
 import AuctionCard from './AuctionCard';
+import { Auction, PagedResult } from '../types';
 
-async function getData() {
+async function getData(): Promise<PagedResult<Auction>> {
     const res = await fetch('http://localhost:6001/search?pageSize=10');
 
     if (!res.ok) throw new Error('Failed to fetch data');
@@ -16,7 +17,7 @@ export default async function Listing() {
   
     return (
         <div className='grid grid-cols-4 gap-6'>
-            {data && data.results.map((auction: any) => (
+            {data && data.results.map(auction => (
                 <AuctionCard auction={auction} key={auction.id} />
             ))}
         </div>
