c96d80c5bf29670675105a852a2d75513c9c7794
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-08-05 08:30:11 -0400

    new - getting access token to authenticate on resource server

--- frontend/web-app/app/actions/AuctionActions.ts
+++ frontend/web-app/app/actions/AuctionActions.ts
@@ -1,6 +1,8 @@
 'use server'
 
+import { getToken } from "next-auth/jwt";
 import { Auction, PagedResult } from "../types";
+import { getTokenWorkaround } from "./authActions";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
     const res = await fetch(`http://localhost:6001/search${query}`);
@@ -15,9 +17,15 @@ export async function UpdateAuctionTest() {
         mileage: Math.floor(Math.random() * 100000) + 1,
     }
 
+    const token = await getTokenWorkaround();
+
     const res = await fetch('http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', {
         method: 'PUT',
-        headers: {}
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + token?.access_token
+        },
+        body: JSON.stringify(data)
     })
 
     if (!res.ok) return {status: res.status, message: res.statusText}
