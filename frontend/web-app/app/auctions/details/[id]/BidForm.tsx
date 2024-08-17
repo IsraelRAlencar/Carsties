684273fcaa2e7081410bdf60e76efca7ee6afa96
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-08-17 15:50:49 -0400

    new - preventing low bids

--- frontend/web-app/app/auctions/details/[id]/BidForm.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -17,6 +17,7 @@ export default function BidForm({auctionId, highBid}: Props) {
     const addBid = useBidStore(state => state.addBid);
 
     function onSubmit(data: FieldValues) {
+        if (data.amount <= highBid) return toast.error('The current minimun Bid is $' + numberWithCommas(highBid + 1));
         placeBidForAuction(auctionId, +data.amount).then(bid => {
             if (bid.error) throw bid.error;
             addBid(bid);
